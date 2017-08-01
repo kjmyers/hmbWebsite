1364a4adbff7399919051900a4f0f2d012936d1e
kjmyers <kevin.j.myers@gmail.com>
2017-08-01 15:44:48 -0400

    Added Map

--- script.js
+++ script.js
@@ -26,3 +26,15 @@ $(document).ready(function () {
 
 });
 
+function initMap() {
+        var uluru = {lat: 39.884045, lng:  -75.531505};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
